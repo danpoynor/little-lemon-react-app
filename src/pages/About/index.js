f865db9730d983b62cdf5fff85f1b6a40c4530bd
Dan Poynor <danpoynor@gmail.com>
2023-05-13 13:46:19 -0500

    Update About page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,16 +1,20 @@
 export default function About() {
   return (
-    <div className="page">
+    <div className="page about-page">
       <div className="container">
         <div className="section-header">
           <h1>About</h1>
         </div>
         <div className="page-content">
-          <p>Little Lemon is a family-owned and operated business. We are a small team of passionate people who love to make delicious food and serve it to our community. We are proud to be a part of the community and we are grateful for the support we have received from our customers.</p>
-          <p>We are a pizza loving family. And for years, I searched and searched and
-        searched for the perfect pizza dough recipe. I tried dozens, or more.
-        And while some were good, none of them were that recipe that would
-        make me stop trying all of the others.</p>
+          <div className="restaurant-images">
+            <img src="photos/restaurant/chef.jpg" alt="inside restaurant" />
+            <img src="photos/restaurant/inside.jpg" alt="inside restaurant" />
+            <img src="photos/restaurant/outside.jpg" alt="outside restaurant" />
+          </div>
+          <div className="restaurant-description">
+            <p><strong>Little Lemon is a family-owned and operated business. We are a small team of passionate people who love to make delicious food and serve it to our community. We are proud to be a part of the community and we are grateful for the support we have received from our customers.</strong></p>
+            <p>We are a pizza loving family. And for years, I searched and searched and searched for the perfect pizza dough recipe. I tried dozens, or more. And while some were good, none of them were that recipe that would make me stop trying all of the others.</p>
+          </div>
         </div>
       </div>
     </div>
