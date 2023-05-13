c000fa4de8d5f986d36a1481b12e94004989649c
Dan Poynor <danpoynor@gmail.com>
2023-05-12 21:21:18 -0500

    Wordsmithing

--- src/pages/Home/sections/Hero.js
+++ src/pages/Home/sections/Hero.js
@@ -8,7 +8,7 @@ export default function SectionHero() {
           <h1>Little Lemon</h1>
           <p className="location">Chicago</p>
           <p className="description">We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-          <Link to="/reservations" className="btn">Reserve a table</Link>
+          <Link to="/reservations" className="btn">Reserve a Table</Link>
         </div>
         <img className="hero-image" src={require('../../../assets/photos/restaurant-food.jpg')} alt="Little Lemon restaurant cuisine" />
       </div>
