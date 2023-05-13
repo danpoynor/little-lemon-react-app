cb4c79a21f5f168e41e957a61dfb70658ecd7c55
Dan Poynor <danpoynor@gmail.com>
2023-05-12 21:19:43 -0500

    Add Menu and Specials menu

--- src/components/cards/Menu.js
+++ src/components/cards/Menu.js
@@ -1,11 +1,12 @@
 export default function MenuCard ({item}) {
   return (
     <figure className="menu-card">
-      <img src={item.image} alt={item.name} className="image" />
+      <div className="photo" style={{ backgroundImage: `url(${item.image})` }}></div>
       <figcaption className="info">
-        <h3 className="name">{item.name}</h3>
-        <p className="description">Description: {item.description}</p>
-        <p className="price">Price: ${item.price}</p>
+        <div className="name-price">
+          <h3 className="name">{item.name}</h3><span className='price'>${item.price}</span>
+        </div>
+        <p className="description">{item.description}</p>
       </figcaption>
     </figure>
   )
--- src/pages/Home/sections/Highlights.js
+++ src/pages/Home/sections/Highlights.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react'
 import { Link } from "react-router-dom";
-import SpecialCard from "../../../components/cards/Special";
+import DataLoader from "../../../utilities/DataLoader";
+import MenuSpecialCard from "../../../components/cards/MenuSpecial";
 
 export default function SectionHighlights() {
+  const [specialItems, setSpecialItems] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch("data/menu.json")
+      const data = await response.json()
+      setSpecialItems(data[0].specials)
+    }
+    getData()
+  }, [])
+
+  if (!specialItems.length) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <section className="highlights">
       <div className="container">
@@ -9,9 +26,17 @@ export default function SectionHighlights() {
           <h2>This weeks specials!</h2>
           <Link to="/menu" className="btn">Online Menu</Link>
         </div>
-        <SpecialCard />
-        <SpecialCard />
-        <SpecialCard />
+        <DataLoader url="data/menu.json" render={(data) => (
+          <ul className="special-menu-items">
+            {
+              data[0] && data[0].specials && data[0].specials.map(item => (
+                <li key={item.id}>
+                  <MenuSpecialCard key={item.id} item={item} />
+                </li>
+              ))
+            }
+          </ul>
+        )} />
       </div>
     </section>
   )
